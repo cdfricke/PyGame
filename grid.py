d3cd6091269e61cb7d7172ed04861dee2caadd69
Connor Fricke <cd.fricke23@gmail.com>
2024-03-06 19:44:36 -0500

    created arrow class for easy way of drawing arrows

--- grid.py
+++ grid.py
@@ -12,7 +12,7 @@ import pygame
 WIDTH = 720
 HEIGHT = 720
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WIDTH+1, HEIGHT+1))
 clock = pygame.time.Clock()
 running = True
 # ******************
@@ -27,18 +27,33 @@ yhat = pygame.Vector2(0, 1)
 # ************************************
 
 # *** CLASS DEFINITIONS ***
-class Grid:
-    # Default grid is a 10 by 10
-    numRows = 10
-    numCols = 10
-    xticks = list(range(0, WIDTH, int(WIDTH / numCols)))
-    yticks = list(range(0, HEIGHT, int(HEIGHT / numRows)))
+# NOTE THAT THESE CLASS DEFINITIONS RELY ON SOME OF THE DEFINED VARIABLES ABOVE
 
+# ARROW : class object for drawing arrows between positions on the screen.
+class Arrow:
+    def __init__(self, tail, tip):
+        self.tip = tip
+        self.tail = tail
+        self.length = (tip - tail).magnitude()
+        self.direction = (tip - tail) / self.length
+        self.perpendicular = pygame.Vector2(self.direction.y, -self.direction.x)
+
+    def draw(self, surface, color, thickness):
+        # central body
+        pygame.draw.line(surface=surface, color=color, start_pos=self.tail, end_pos=self.tip, width=thickness)
+        # angled tip
+        leftAngled = (0.98 * self.length * self.direction) + (0.02 * self.length * self.perpendicular)
+        rightAngled = (0.98 * self.length * self.direction) - (0.02 * self.length * self.perpendicular)
+        points = [leftAngled, self.tip, rightAngled]
+        pygame.draw.lines(surface=surface, color=color, closed=False, points=points, width=thickness)
+
+# GRID: class object for drawing a grid with a certain number of rows and columns on the screen.
+class Grid:
     def __init__(self, rows, columns):
         self.numRows = rows
         self.numCols = columns
-        self.xticks = list(range(0, WIDTH, int(WIDTH / columns)))
-        self.yticks = list(range(0, HEIGHT, int(HEIGHT / rows)))
+        self.xticks = list(range(0, WIDTH + 1, int(WIDTH / columns)))
+        self.yticks = list(range(0, HEIGHT + 1, int(HEIGHT / rows)))
     
     def draw(self, surface, color, thickness):
         for tick in self.xticks:
@@ -52,16 +67,19 @@ class Grid:
         
 # *************************
 
-gameMap = Grid(5, 5)
+# rows, cols
+gameMap = Grid(3, 3)
+centerArrow = Arrow(origin, center - 300*xhat)
+
 
 # *** GAME LOOP ***
 while (running):
-
     # wipe away anything from the previous frame
     screen.fill("black")
 
     # *** RENDER THE GAME HERE ***
     gameMap.draw(screen, "white", 1)
+    centerArrow.draw(screen, "white", 3)
 
     # *** END RENDERING ***
 
